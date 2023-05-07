abdac636981d630a54c51858c44843521560c1a7
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-07 17:03:24 +0300

    Created project

---
+++ Linked Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Linked Animation
+//
+//  Created by Sergei Semko on 5/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
